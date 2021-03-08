6f59a959a7cee82efc5c5dbe8b8f96227a72688b
Roberto <robertos@jccm.es>
2021-03-08 13:49:01 +0100

    Terminadas las directivas

--- src/app/pages/directivas/directivas.component.ts
+++ src/app/pages/directivas/directivas.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directivas.component.css']
 })
 export class DirectivasComponent implements OnInit {
+  titulo='Centros educativos';
+  colegio='Colegios Infantes';
+  mostrarColegio=false;
+  datos=[1,2,3,4,5];
+  colegios: string[]=['Nara','Infantes','Mayol','Sefarad'];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarOcultar():void {
+    this.mostrarColegio=!this.mostrarColegio;
+  }
 }
